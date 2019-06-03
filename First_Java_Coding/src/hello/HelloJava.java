4b6a40a71d5972750ee034132069f5d256fdeee9
larryback <41230266+larryback@users.noreply.github.com>
2019-06-03 16:18:52 +0900

    6월3일오후

---
+++ First_Java_Coding/src/hello/HelloJava.java
@@ -0,0 +1,17 @@
+/*
+ * Date: 2019년 6월 1일
+ * Author: 백영래
+ * Description: 첫 번째 자바 프로그램입니다.
+ * Version: 1.0
+ */
+
+
+package hello;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+	}
+
+}
