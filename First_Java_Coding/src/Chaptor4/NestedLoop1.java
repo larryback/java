669d493e8eb0e66708778562403f721676796ff5
larryback <41230266+larryback@users.noreply.github.com>
2019-06-20 22:59:43 +0900

    6월20일저녁

---
+++ First_Java_Coding/src/Chaptor4/NestedLoop1.java
@@ -0,0 +1,18 @@
+package Chaptor4;
+
+public class NestedLoop1 {
+
+	public static void main(String[] args) {
+		int dan;
+		int times;
+		
+		for(dan = 3; dan <=7; dan++){
+			for(times = 1; times <=9; times++){
+				System.out.println(dan + "X" + times + "=" + dan * times);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
