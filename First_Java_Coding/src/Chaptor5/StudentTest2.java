424806fadf85405498ae60a8e9c5d33cd6261ec5
larryback <41230266+larryback@users.noreply.github.com>
2019-06-27 01:03:46 +0900

    6월27일새벽

---
+++ First_Java_Coding/src/Chaptor5/StudentTest2.java
@@ -0,0 +1,17 @@
+package Chaptor5;
+
+public class StudentTest2 {
+
+	public static void main(String[] args) {
+		Student3 student1 = new Student3();
+		student1.studentName = "안연수"; 
+				
+		Student3 student2 = new Student3();
+		student2.studentName = "안승연"; 
+		
+		System.out.println(student1);
+		System.out.println(student2);
+
+	}
+
+}
