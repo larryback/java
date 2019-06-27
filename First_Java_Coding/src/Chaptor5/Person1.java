3d9b77d58e68df4d9a89933df508dce6d245ed30
larryback <41230266+larryback@users.noreply.github.com>
2019-06-28 00:04:18 +0900

    6월28일새벽

---
+++ First_Java_Coding/src/Chaptor5/Person1.java
@@ -0,0 +1,21 @@
+package Chaptor5;
+
+public class Person1 {
+
+	  String name;
+	  float height;
+	  float weight;
+	  
+	  public Person1() {}
+	
+	  public Person1(String pname) {
+		  name = pname;
+	  }
+	  public Person1(String pname, float pheight, float pweight) {
+		  
+		  name = pname;
+		  height = pheight;
+		  weight = pweight;
+		  
+	  }
+}
