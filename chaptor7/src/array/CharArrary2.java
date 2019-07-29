b4436f8973898a6b472b068e5bd3c8a3bb44ce70
larryback <41230266+larryback@users.noreply.github.com>
2019-07-29 22:55:20 +0900

    7월29일 저녁

---
+++ chaptor7/src/array/CharArrary2.java
@@ -0,0 +1,23 @@
+package array;
+
+public class CharArrary2 {
+	public static void main(String[] args) {
+
+		char[] alpahabets = new char[26];
+		char ch = 'A';
+		
+		for(int i=0 ; i<alpahabets.length; i++, ch++){
+			alpahabets[i] = ch;
+		}
+		
+		for(int i=0; i<alpahabets.length; i++){
+			alpahabets[i] = (char)( alpahabets[i] + 32);
+		}
+		
+		for(int i=0 ; i<alpahabets.length; i++, ch++){
+			System.out.println(alpahabets[i]);
+		}
+		
+	}
+
+}
