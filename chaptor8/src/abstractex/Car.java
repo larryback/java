34c07382db487ba5fa4fe4de776f946d3399dfee
larryback <41230266+larryback@users.noreply.github.com>
2019-08-10 23:06:22 +0900

    8월10일저녁

---
+++ chaptor8/src/abstractex/Car.java
@@ -0,0 +1,10 @@
+package abstractex;
+
+public abstract class  Car {
+	public abstract void run();
+	public abstract void refuel();
+	public void stop() {
+		System.out.println("Â÷°¡ ¸ØÃä´Ï´Ù.");
+	}
+
+}
