bab8ec1406d781d66a7c443d1a6c92c6f29d033c
larryback <41230266+larryback@users.noreply.github.com>
2019-08-28 23:41:36 +0900

    8월28일 저녁

---
+++ chapter10/src/privatee/Calculator.java
@@ -0,0 +1,32 @@
+package privatee;
+
+public  class Calculator implements Calc{
+	@Override
+	public int add(int num1, int num2) {
+		return num1 + num2;
+	}
+
+	@Override
+	public int substract(int num1, int num2) {
+		return num1 - num2;
+	}
+
+	@Override
+	public int times(int num1, int num2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int divide(int num1, int num2) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void myMethod() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
