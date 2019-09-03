08b7c87537f23d9f740de0c9726de7dd15bbe027
larryback <41230266+larryback@users.noreply.github.com>
2019-09-03 23:44:53 +0900

    9월3일 저녁

---
+++ chapter11/src/object/ToStringEX.java
@@ -0,0 +1,12 @@
+package object;
+
+public class ToStringEX {
+	public static void main(String[] args) {
+
+		Book book1 = new Book(200, "°³¹Ì");
+		
+		System.out.println(book1);
+		System.out.println(book1.toString());
+		}
+
+}
