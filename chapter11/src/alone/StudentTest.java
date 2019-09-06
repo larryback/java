35624c9779586a747dbe0133900af25d82f9b3b5
larryback <41230266+larryback@users.noreply.github.com>
2019-09-06 23:12:13 +0900

    9월6일 저녁

---
+++ chapter11/src/alone/StudentTest.java
@@ -0,0 +1,9 @@
+package alone;
+
+public class StudentTest {
+	public static void main(String[] args) {
+		Student studentKim = new Student(1001, "±èÀ¯½Å");
+		System.out.println(studentKim);
+	}
+
+}
